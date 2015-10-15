8a90e8aaf46ebaa3608ce4d7324782a637133b66
Matthew Hasbach <hasbach.git@gmail.com>
2015-10-14 23:01:36 -0400

    Fix user edit regression

--- ui/js/controllers/admin_users_search.js
+++ ui/js/controllers/admin_users_search.js
@@ -1,6 +1,13 @@
+import pluck from 'lodash/collection/pluck';
+
 class AdminUsersSearchController {
     constructor($scope, search) {
         search($scope, {model: 'user', orderBy: 'id'});
+
+        $scope.edit = function() {
+            $scope.editQueue.ids = pluck($scope.selected, 'id');
+            $scope.tabs.i = 1;
+        };
     }
 }
 
