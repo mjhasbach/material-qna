2395b402d3c26bf3cf918a39f408425b891bacf3
Matthew Hasbach <hasbach.git@gmail.com>
2015-10-20 01:58:45 -0400

    Replace google-images2 with google-image

--- server/image_search.js
+++ server/image_search.js
@@ -1,26 +1,20 @@
 "use strict";
 
-let googleImages = require('google-images2');
+let googleImage = require('google-image');
 
 module.exports = function(req, cb) {
-    googleImages.search(
-        req.query.query,
-        {
-            page: req.query.index,
-            rawQuery: {
-                rsz: 1,
-                safe: 'active',
-                imgsz: 'large',
-                userip: req.ip
-            },
-            callback: function(err, images) {
-                if (err) {
-                    return cb(err);
-                }
+    new googleImage(req.query.query)
+        .page(req.query.index)
+        .options({
+            safe: 'active',
+            imgsz: 'large',
+            userip: req.ip
+        })
+        .search()
+        .then(function(images) {
+            let image = images[0];
 
-                let image = images[0];
-
-                image ? cb(null, image.url) : cb(new Error('No images found'));
-            }
-        });
+            image ? cb(null, image.url) : cb(new Error('No images found'));
+        })
+        .catch(cb);
 };
