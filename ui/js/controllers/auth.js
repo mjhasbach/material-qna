9f1a6038a840dd155be612b0a6cafac2f723123a
Matthew Hasbach <hasbach.git@gmail.com>
2015-12-19 01:19:42 -0500

    Add enter to submit login behavior again

--- ui/js/controllers/auth.js
+++ ui/js/controllers/auth.js
@@ -1,23 +1,22 @@
 export default class {
-    constructor($scope, $timeout, auth, toast) {
+    constructor($scope, auth, toast) {
         'ngInject';
 
         Object.assign($scope, {
             areButtonsDisabled() {
                 return $scope.authenticating || !$scope.username || !$scope.password;
             },
-            authenticate(authForm, isRegistering) {
+            authenticate(isRegistering) {
                 auth.authenticate(isRegistering, $scope.username, $scope.password).then(function(response) {
                     $scope.user.data = response.data;
                     $scope.view.current = 'qna';
                 }).catch(function() {
                     toast.show(`Unable to ${isRegistering ? 'register' : 'log in'}`);
                 }).finally(function() {
-                    authForm.$setUntouched();
+                    $scope.authForm.$setUntouched();
+                    $scope.authForm.$setPristine();
                     $scope.authenticating = $scope.registering = $scope.loggingIn = false;
-                    $timeout(function() {
-                        $scope.password = null;
-                    }, 0);
+                    $scope.password = null;
                 });
 
                 Object.assign($scope, {
